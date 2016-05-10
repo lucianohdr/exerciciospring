dcb264c1d8c486e320472a44c8992a994775c2e0
Luciano Heleno da Rosa <lucianohdr@gmail.com>
2016-05-09 23:24:03 -0300

    models e jpa

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Id
+	@SequenceGenerator(sequenceName="employee_id_gen", name = "employee_id_gen")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="employee_id_gen")
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	private BigDecimal salary;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getSalary() {
+		return salary;
+	}
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+}
