ce0a9f0c02711387c499c267ddc62e1c22147ffb
Luciano Heleno da Rosa <lucianohdr@gmail.com>
2016-05-11 22:39:42 -0300

    Alterações em project e removendo pasta target do index do git

--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,11 +1,25 @@
 package model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name="project")
 public class Project {
 
+	@Id
+	@SequenceGenerator(sequenceName="project_id_gen", name = "project_id_gen")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="project_id_gen")
 	private Long id;
+	
+	@Column(name="name")
+	@Size(min=2, max=300)
 	private String name;
 	
 	public Long getId() {
